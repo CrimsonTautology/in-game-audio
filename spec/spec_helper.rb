395f1d8622965792c00025e83502dcb2ac9302e1
??? <billehs+github@gmail.com>
2014-01-17 13:24:44 -0500

    Setup plugins/rspec

---
+++ spec/spec_helper.rb
@@ -0,0 +1,76 @@
+# This file is copied to spec/ when you run 'rails generate rspec:install'
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+require 'capybara/rspec'
+require 'rspec/autorun'
+
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+
+#Configure omniauth
+OmniAuth.config.test_mode = true
+OmniAuth.config.mock_auth[:steam] = {
+  uid: '12345',
+  info: {
+    nickname: 'Example User [TEST]',
+    image: 'http://placehold.it/350x150',
+    urls: {
+      Profile: 'http://example.com'
+    }
+  }
+}
+
+RSpec.configure do |config|
+  # ## Mock Framework
+  #
+  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
+  #
+  # config.mock_with :mocha
+  # config.mock_with :flexmock
+  # config.mock_with :rr
+
+  # If you're not using ActiveRecord, or you'd prefer not to run each of your
+  # examples within a transaction, remove the following line or assign false
+  # instead of true.
+  config.use_transactional_fixtures = true
+
+  # If true, the base class of anonymous controllers will be inferred
+  # automatically. This will be the default behavior in future versions of
+  # rspec-rails.
+  config.infer_base_class_for_anonymous_controllers = false
+
+  # Run specs in random order to surface order dependencies. If you find an
+  # order dependency and want to debug it, you can fix the order by providing
+  # the seed, which is printed after each run.
+  #     --seed 1234
+  config.order = "random"
+
+  #Use new expect syntax
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+
+  #Setup factorygirl
+  config.include FactoryGirl::Syntax::Methods
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+
+  config.include AuthMacros
+  config.include Requests::JsonHelpers, type: :request
+end
