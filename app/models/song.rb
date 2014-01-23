6f4ac19284684367f702d082a7d2b40a599157aa
??? <billehs+github@gmail.com>
2014-01-23 14:41:52 -0500

    Add create_from_full_path
    quickly add song files with a directory structure

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,4 +2,21 @@ class Song < ActiveRecord::Base
   belongs_to :directory
 
   validates :name, uniqueness: { scope: :directory_id, message: "A directory cannot have two songs of the same name", case_sensitive: false}
+
+
+
+  def self.create_from_full_path full_path
+    directories = full_path.gsub(/\A\s*\//, "").gsub(/\/\s*\z/, "").split("/")
+    name = directories.pop
+
+    parent = Directory.root
+    directories.each do |dir|
+      parent = parent.find_or_create_subdirectory dir
+    end
+
+    Song.create do |s|
+      s.name = name
+      s.directory = parent
+    end.save
+  end
 end
