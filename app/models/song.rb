cef8d8cf343460400477d7b5f469ec25bdb492c7
??? <billehs+github@gmail.com>
2014-01-23 14:51:56 -0500

    Add method to get song's full path

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,7 +3,17 @@ class Song < ActiveRecord::Base
 
   validates :name, uniqueness: { scope: :directory_id, message: "A directory cannot have two songs of the same name", case_sensitive: false}
 
+  def full_path
+    parent = directory
+    path = name
 
+    while !parent.root? do
+      path = "#{parent.name}/#{path}" 
+      parent = parent.parent
+    end
+
+    path
+  end
 
   def self.create_from_full_path full_path
     directories = full_path.gsub(/\A\s*\//, "").gsub(/\/\s*\z/, "").split("/")
