6686ac70ceadd0d814b4f6c8855e545b74e493be
??? <billehs+github@gmail.com>
2014-01-24 11:04:45 -0500

    Add factory definitions

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,3 +1,20 @@
 FactoryGirl.define do
 
+  factory :directory do
+    name "foo"
+    parent :root
+    root false
+  end
+
+  factory :root, class: Directory do
+    name ""
+    parent nil
+    root true
+  end
+
+  factory :song do
+    name "baz"
+    directory :root
+  end
+
 end
