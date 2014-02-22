d3ab056a0fc3a741101cb7a85e25fd0c7313fe4d
The Crimson Tautology <billehs+github@gmail.com>
2014-02-22 13:36:54 -0500

    Fix typo in sound_content_type

--- db/migrate/20140222182350_add_missing_paperclip_fields_to_songs.rb
+++ db/migrate/20140222182350_add_missing_paperclip_fields_to_songs.rb
@@ -2,6 +2,6 @@ class AddMissingPaperclipFieldsToSongs < ActiveRecord::Migration
   def change
     add_column :songs, :sound_file_name, :string
     add_column :songs, :sound_file_size, :integer
-    add_column :songs, :sound_contnet_type, :string
+    add_column :songs, :sound_content_type, :string
   end
 end
