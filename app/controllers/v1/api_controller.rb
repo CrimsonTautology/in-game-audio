5ff5521503ed35a10ff880355bd7c775cf798327
??? <billehs+github@gmail.com>
2014-03-05 16:18:49 -0500

    Have api return song information

--- app/controllers/v1/api_controller.rb
+++ app/controllers/v1/api_controller.rb
@@ -1,14 +1,31 @@
 module V1
   class ApiController < ApplicationController
     #authorize_resource class: false
+    before_filter :check_api_key
     before_filter :check_path, only: [:song_query]
     respond_to :json
 
     def song_query
-      out = {
-        found: false,
-        command: "song_query"
-      }
+      song = Song.path_search @path
+      if song.nil?
+        out = {
+          found: false,
+          command: "song_query"
+        }
+      else
+        out = {
+          found: true,
+          command: "song_query",
+          song_id: song.id,
+          full_path: song.full_path,
+          title: song.title,
+          album: song.album,
+          artist: song.artist,
+          duration: song.duration,
+          duration_formated: song.duration_formated
+        }
+
+      end
       render json: out
     end
 
