fbe51dc1c3e264596a2a691a8dab5983602f5a96
The Crimson Tautology <billehs+github@gmail.com>
2014-03-09 16:43:57 -0400

    touch up initializer

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,20 +1,15 @@
-if Rails.env.test? or Rails.env.cucumber?
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    provider:                         'Google',
+    google_storage_access_key_id:     ENV['GOOGLE_STORAGE_ACCESS_KEY_ID'] || "",
+    google_storage_secret_access_key: ENV['GOOGLE_STORAGE_SECRET_ACCESS_KEY'] || ""
+  }
+  config.fog_directory = ENV['GOOGLE_STORAGE_DIRECTORY'] || ""
 
-  CarrierWave.configure do |config|
+  if Rails.env.test? or Rails.env.cucumber?
     config.storage = :file
     config.enable_processing = false
-  end
-
-else
-
-  CarrierWave.configure do |config|
-    config.fog_credentials = {
-      provider:                         'Google',
-      google_storage_access_key_id:     ENV['GOOGLE_STORAGE_ACCESS_KEY_ID'],
-      google_storage_secret_access_key: ENV['GOOGLE_STORAGE_SECRET_ACCESS_KEY']
-    }
-    config.fog_directory = ENV['GOOGLE_STORAGE_DIRECTORY']
+  else
     config.storage = :fog
   end
-
 end
