970d464fb3fcb308fa4a6f8f4c85210cd563c9ae
??? <billehs+github@gmail.com>
2014-03-26 14:20:52 -0400

    Restructure javascript files

---
+++ app/assets/javascripts/play.js
@@ -0,0 +1,33 @@
+$( document ).ready(function(){
+    //jPlayer - Default to auto play
+    var player = $("#audio_player");
+    var url = player.data('url');
+    var vol = player.data('volume');
+    var seek= player.data('seek');
+
+    player.jPlayer({
+      ready: function(event) {
+        $(this).jPlayer("setMedia", {
+          mp3: url
+        }).jPlayer("play", seek);
+      },
+      supplied: "mp3",
+      swfPath: "/",
+      volume: vol,
+      preload: "auto"
+    });
+
+    //URL Anchor Listener
+    window.hashchange(hashchanged);
+    hashchanged();
+}
+
+function hashchanged(){
+    //change volume or position without reloading page
+    var hash = window.location.hash
+
+    $("#audio_player").jPlayer("volume", hash);
+
+    //TODO add seek functionality
+    //$("#audio_player").jPlayer("play", seek);
+}
