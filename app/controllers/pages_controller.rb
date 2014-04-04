1d2a48d7b0c8b8d274574995e293d2d33222fabb
??? <billehs+github@gmail.com>
2014-04-04 12:27:33 -0400

    Add some statistics to front page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,9 @@
 class PagesController < ApplicationController
   def home
+    limit = params["limit"] || 25
+    @recently_uploaded = Song.includes(:uploader).order(created_at: :desc).limit(limit)
+    @recently_played = PlayEvent.includes(:song, :user, :api_key).where(type_of: "pall").order(created_at: :desc).limit(limit)
+    @trending
   end
 
   def help
