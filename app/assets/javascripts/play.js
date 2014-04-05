8043c7f2e1e664cf58e0670ad99d6c28e8e3b304
The Crimson Tautology <billehs+github@gmail.com>
2014-04-05 15:41:21 -0400

    Switch to soundmanager2

--- app/assets/javascripts/play.js
+++ app/assets/javascripts/play.js
@@ -1,20 +1,21 @@
 $( document ).ready(function(){
-    //jPlayer - Default to auto play
+
     var player = $("#audio_player");
     var url = player.data('url');
     var vol = player.data('volume');
     var seek= player.data('seek');
 
-    player.jPlayer({
-      ready: function(event) {
-        $(this).jPlayer("setMedia", {
-          mp3: url
-        }).jPlayer("play", seek);
-      },
-      supplied: "mp3",
-      swfPath: "/",
-      volume: vol,
-      preload: "auto"
+    soundManager.setup({
+        url: '/swf/',
+        flashVersion: 8, // optional: shiny features (default = 8)
+        preferFlash: true,
+        onready: function() {
+            soundManager.createSound({
+                id: 'play',
+                url: url,
+                volume: vol
+            }).play();
+        }
     });
 
     //URL Anchor Listener
@@ -26,7 +27,7 @@ function hashchanged(){
     //change volume or position without reloading page
     var hash = window.location.hash
 
-    $("#audio_player").jPlayer("volume", hash);
+        $("#audio_player").jPlayer("volume", hash);
 
     //TODO add seek functionality
     //$("#audio_player").jPlayer("play", seek);
