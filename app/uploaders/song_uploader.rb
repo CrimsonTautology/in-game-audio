cf5bb5499802d270f434576df37aecacfdeaa52c
??? <billehs+github@gmail.com>
2014-05-06 14:44:47 -0400

    upload different mp3 and ogg version

--- app/uploaders/song_uploader.rb
+++ app/uploaders/song_uploader.rb
@@ -3,15 +3,81 @@ require 'audioinfo'
 
 class SongUploader < CarrierWave::Uploader::Base
   include CarrierWave::MimeTypes
-  # Choose what kind of storage to use for this uploader:
-  # storage :file
-  # storage :fog
 
   process :extract_original_file_details
-  #process :convert_to_ogg
-  process :convert_to_mp3
-  process :set_content_type
-  process :extract_converted_file_details
+
+  version :mp3 do
+    process :convert_to_mp3
+    process :extract_converted_file_details
+    process :set_content_type
+
+    def full_filename(for_file)
+      model.sound_file_name + '.mp3'
+    end
+
+    def set_content_type(*args)
+      self.file.instance_variable_set(:@content_type, "audio/mp3")
+    end
+
+    def convert_to_mp3
+      #ffmpeg -y -i @file -vn -sn -aq 6 -ar 44100 -ac 2 @out.mp3
+      cache_stored_file! if !cached?
+
+      directory = File.dirname current_path
+      tmp_path  = File.join directory, "tmpfile"
+      File.rename current_path,  tmp_path
+
+      file = FFMPEG::Movie.new(tmp_path)
+      opts={
+        audio_codec: "libmp3lame",
+        audio_bitrate: 32,
+        audio_sample_rate: 22050,
+        audio_channels: 1,
+        custom: "-vn -aq 4 -sn -f mp3"
+      }
+      file.transcode(current_path, opts)
+
+      File.delete tmp_path
+    end
+
+
+  end
+
+  version :ogg do
+    process :convert_to_ogg
+    process :set_content_type
+
+    def full_filename(for_file)
+      model.sound_file_name + '.ogg'
+    end
+
+    def set_content_type(*args)
+      self.file.instance_variable_set(:@content_type, "audio/ogg")
+    end
+
+    def convert_to_ogg
+      #ffmpeg -y -i @file -acodec libvorbis -vn -sn -aq 6 -ar 44100 -ac 2 @out.ogg
+      cache_stored_file! if !cached?
+
+      directory = File.dirname current_path
+      tmp_path  = File.join directory, "tmpfile"
+      File.rename current_path,  tmp_path
+
+      file = FFMPEG::Movie.new(tmp_path)
+      opts={
+        audio_codec: "libvorbis",
+        audio_bitrate: 32,
+        audio_sample_rate: 22050,
+        audio_channels: 1,
+        custom: "-vn -sn -f ogg"
+      }
+      file.transcode(current_path, opts)
+
+      File.delete tmp_path
+    end
+
+
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -22,10 +88,6 @@ class SongUploader < CarrierWave::Uploader::Base
   # Add a white list of extensions which are allowed to be uploaded.
   def extension_white_list
     %w(mp3 mp4 ogg wav)
-  end
-
-  def full_filename(for_file)
-    model.sound_file_name + '.mp3'
   end
 
 
@@ -44,53 +106,6 @@ class SongUploader < CarrierWave::Uploader::Base
   def extract_converted_file_details
     model.sound_content_type = file.content_type if file.content_type
     model.sound_file_size = file.size
-  end
-
-  def convert_to_ogg
-    #ffmpeg -y -i @file -acodec libvorbis -vn -sn -aq 6 -ar 44100 -ac 2 @out.ogg
-    cache_stored_file! if !cached?
-
-    directory = File.dirname current_path
-    tmp_path  = File.join directory, "tmpfile"
-    File.rename current_path,  tmp_path
-
-    file = FFMPEG::Movie.new(tmp_path)
-    opts={
-      audio_codec: "libvorbis",
-      audio_bitrate: 32,
-      audio_sample_rate: 22050,
-      audio_channels: 1,
-      custom: "-vn -sn -f ogg"
-    }
-    file.transcode(current_path, opts)
-
-    File.delete tmp_path
-  end
-
-  def convert_to_mp3
-    #ffmpeg -y -i @file -vn -sn -aq 6 -ar 44100 -ac 2 @out.mp3
-    cache_stored_file! if !cached?
-
-    directory = File.dirname current_path
-    tmp_path  = File.join directory, "tmpfile"
-    File.rename current_path,  tmp_path
-
-    file = FFMPEG::Movie.new(tmp_path)
-    opts={
-      audio_codec: "libmp3lame",
-      audio_bitrate: 32,
-      audio_sample_rate: 22050,
-      audio_channels: 1,
-      custom: "-vn -aq 4 -sn -f mp3"
-    }
-    file.transcode(current_path, opts)
-
-    File.delete tmp_path
-  end
-
-  def set_content_type(*args)
-    #FIXME Is there a better way to do this?
-    self.file.instance_variable_set(:@content_type, "audio/mp3")
   end
 
 end
