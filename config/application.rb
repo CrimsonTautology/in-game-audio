4d4507f4f162ef1b749ca45ea38c40076a674975
??? <billehs+github@gmail.com>
2014-10-03 11:41:13 -0400

    Set time zone

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,6 @@ module InGameAudio
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.i18n.enforce_available_locales = false
+    config.time_zone = 'Eastern Time (US & Canada)'
   end
 end
