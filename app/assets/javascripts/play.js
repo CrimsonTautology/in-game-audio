7f805877b1e64e902b882fdb65fb9855f9ca190e
The Crimson Tautology <billehs+github@gmail.com>
2015-04-25 14:41:58 -0400

    Add ability to live change volume with hash param
    This allows players in game to change volume without having to restart the song

--- app/assets/javascripts/play.js
+++ app/assets/javascripts/play.js
@@ -1,11 +1,11 @@
 $( document ).ready(function(){
 
-    var player = $("#audio_player");
+    var player = $('#audio_player');
     var url = player.data('url');
     var vol = player.data('volume');
     var seek= player.data('seek');
 
-    player.prop("volume", vol);
+    player.prop('volume', vol);
 
     //URL Anchor Listener
     window.onhashchange = hashchanged;
@@ -14,8 +14,24 @@ $( document ).ready(function(){
 
 function hashchanged(){
     //change volume or position without reloading page
-    var player = $("#audio_player");
-    var hash = window.location.hash
+    var player = $('#audio_player');
+    var hash = window.location.hash;
 
-    player.prop("volume", hash);
+
+    if(hash.lastIndexOf('#v=', 0) === 0){
+        //We're changing volume
+        var newVolume = parseVolume(hash);
+        newVolume = adjustVolume(newVolume);
+        player.prop('volume', newVolume);
+    }
+
+
+}
+
+function parseVolume(unparsed){
+    return unparsed.replace('#v=', '');
+}
+
+function adjustVolume(unadjusted){
+    return Math.pow(unadjusted, 4.0);
 }
